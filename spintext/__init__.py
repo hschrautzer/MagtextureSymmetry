894864a019f36ebae9fcea4ee549ff16faa89b44
hschrautzer <h.schrautzer@gmail.com>
2026-02-23 10:39:54 +0100

    Create __init__.py

---
+++ spintext/__init__.py
@@ -0,0 +1,11 @@
+"""
+Public API for the spintext package.
+"""
+
+from .texture import SpinTexture
+from .symmetry import TextureSymmetry
+
+__all__ = [
+    "SpinTexture",
+    "TextureSymmetry"
+]
